a81fc41bef46d2b719d2f6f905de3b3b1e5f88f4
Rangga Dikarinata <dikarinata@gmail.com>
2023-04-01 20:21:22 +0700

    feat: Add Top Rated Tv Notifier and test file

---
+++ lib/presentation/tv/provider/top_rated_tvs_notifier.dart
@@ -0,0 +1,38 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/usecases/tv/get_popular_tvs.dart';
+import 'package:ditonton/domain/usecases/tv/get_top_rated_tvs.dart';
+import 'package:flutter/foundation.dart';
+
+import '../../../domain/entities/tv.dart';
+
+class TopRatedTvsNotifier extends ChangeNotifier {
+  final GetTopRatedTvs getTopRatedTvs;
+
+  TopRatedTvsNotifier(this.getTopRatedTvs);
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<Tv> _tvs = [];
+  List<Tv> get tv => _tvs;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchTopRatedTvs() async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result = await getTopRatedTvs.execute();
+
+    result.fold((failure) {
+      _message = failure.message;
+      _state = RequestState.Error;
+      notifyListeners();
+    }, (data) {
+      _tvs = data;
+      _state = RequestState.Loaded;
+      notifyListeners();
+    });
+  }
+}
