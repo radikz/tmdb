40691ded08858e074c4a7c5f0479a502739b0c43
Rangga Dikarinata <dikarinata@gmail.com>
2023-04-11 09:14:22 +0700

    test: search tv

---
+++ test/domain/usecases/tv/search_tvs_test.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/tv/get_popular_tvs.dart';
+import 'package:ditonton/domain/usecases/tv/get_tv_detail_episode.dart';
+import 'package:ditonton/domain/usecases/tv/search_tvs.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import '../../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late SearchTvs searchTvs;
+  late MockTvRepository repository;
+
+  setUp(() {
+    repository = MockTvRepository();
+    searchTvs = SearchTvs(repository);
+  });
+
+  final listTv = <Tv>[testTv];
+
+  test('get list tv', () async {
+    when(repository.searchTvs("tv"))
+        .thenAnswer((_) async => Right(listTv));
+
+    final result = await searchTvs.execute("tv");
+
+    expect(result, Right(listTv));
+  });
+}
