b39bc668377851eadaec1da2cf15dbf78f88985b
Rangga Dikarinata <dikarinata@gmail.com>
2023-04-11 10:05:06 +0700

    test: save watchlist tv

---
+++ test/domain/usecases/tv/save_watchlist_tv_test.dart
@@ -0,0 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/usecases/tv/remove_watchlist_tv.dart';
+import 'package:ditonton/domain/usecases/tv/save_watchlist_tv.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import '../../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late SaveWatchlistTv saveWatchlistTv;
+  late MockTvRepository repository;
+
+  setUp(() {
+    repository = MockTvRepository();
+    saveWatchlistTv = SaveWatchlistTv(repository);
+  });
+
+  test('save watchlist tv', () async {
+    when(repository.saveWatchlist(testTvDetail)).thenAnswer((_) async => Right("Added to Watchlist"));
+
+    final result = await saveWatchlistTv.execute(testTvDetail);
+
+    expect(result, Right("Added to Watchlist"));
+  });
+}
