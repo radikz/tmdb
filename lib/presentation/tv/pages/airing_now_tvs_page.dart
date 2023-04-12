db8e784c61c7b6aa0f1c2c218d1e0a0af81e4f26
Rangga Dikarinata <dikarinata@gmail.com>
2023-04-12 13:19:31 +0700

    feat: add airing now page tv

---
+++ lib/presentation/tv/pages/airing_now_tvs_page.dart
@@ -0,0 +1,56 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/tv/pages/widgets/tv_card_list.dart';
+import 'package:ditonton/presentation/tv/provider/airing_now_tvs_notifier.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AiringNowTvsPage extends StatefulWidget {
+  static const ROUTE_NAME = '/popular-movie';
+
+  @override
+  _AiringNowTvsPageState createState() => _AiringNowTvsPageState();
+}
+
+class _AiringNowTvsPageState extends State<AiringNowTvsPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() =>
+        Provider.of<AiringNowTvsNotifier>(context, listen: false)
+            .fetchAiringNowTvs());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Airing Now Tvs'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Consumer<AiringNowTvsNotifier>(
+          builder: (context, data, child) {
+            if (data.state == RequestState.Loading) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (data.state == RequestState.Loaded) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final tv = data.tv[index];
+                  return TvCard(tv);
+                },
+                itemCount: data.tv.length,
+              );
+            } else {
+              return Center(
+                key: Key('error_message_tv'),
+                child: Text(data.message),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
