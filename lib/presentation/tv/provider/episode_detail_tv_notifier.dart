7a5738a6dcd3445faad05024fcef63451cd136ef
Rangga Dikarinata <dikarinata@gmail.com>
2023-04-25 13:18:33 +0700

    feat: add episode detail tv notifier and test

---
+++ lib/presentation/tv/provider/episode_detail_tv_notifier.dart
@@ -0,0 +1,40 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_episode.dart';
+import 'package:ditonton/domain/usecases/tv/get_tv_detail_episode.dart';
+import 'package:flutter/foundation.dart';
+
+class EpisodeDetailTvNotifier extends ChangeNotifier {
+  final GetTvDetailEpisode getTvDetailEpisode;
+
+  EpisodeDetailTvNotifier(this.getTvDetailEpisode);
+
+  late TvEpisode _episodes;
+  TvEpisode get episodes => _episodes;
+
+  String _message = "";
+  String get message => _message;
+
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  Future<void> fetchEpisode(
+      {required int tvId,
+      required int seasonNumber,
+      required int episodeNumber}) async {
+    _state = RequestState.Loading;
+    notifyListeners();
+
+    final result =
+        await getTvDetailEpisode.execute(tvId, seasonNumber, episodeNumber);
+
+    result.fold((failure) {
+      _state = RequestState.Error;
+      _message = failure.message;
+      notifyListeners();
+    }, (data) {
+      _episodes = data;
+      _state = RequestState.Loaded;
+      notifyListeners();
+    });
+  }
+}
