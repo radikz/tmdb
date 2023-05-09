39e9019927ef8469fb1503f6fede8f18fd4799f6
Rangga Dikarinata <dikarinata@gmail.com>
2023-05-09 07:28:18 +0700

    refactor: modularzation about page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:about/about_page.dart';
 import 'package:ditonton/common/constants.dart';
 import 'package:ditonton/common/utils.dart';
-import 'package:ditonton/presentation/pages/about_page.dart';
 import 'package:ditonton/presentation/pages/movie_detail_page.dart';
 import 'package:ditonton/presentation/pages/home_movie_page.dart';
 import 'package:ditonton/presentation/pages/popular_movies_page.dart';
