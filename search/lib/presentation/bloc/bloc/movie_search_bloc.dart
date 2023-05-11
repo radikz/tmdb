4a3eafc540cd5664c59a2bd5bf423a73af4a77c3
Rangga Dikarinata <dikarinata@gmail.com>
2023-05-11 11:27:29 +0700

    feat: change provider to MovieSearchBloc

---
+++ search/lib/presentation/bloc/bloc/movie_search_bloc.dart
@@ -0,0 +1,34 @@
+import 'package:bloc/bloc.dart';
+import 'package:core/movie/domain/entities/movie.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:search/domain/usecases/search_movies.dart';
+
+
+part 'movie_search_event.dart';
+part 'movie_search_state.dart';
+
+EventTransformer<T> debounce<T>(Duration duration) {
+  return (events, mapper) => events.debounceTime(duration).flatMap(mapper);
+}
+
+class MovieSearchBloc extends Bloc<MovieSearchEvent, MovieSearchState> {
+  MovieSearchBloc(this._searchMovies) : super(MovieSearchEmpty()) {
+    on<OnQueryChanged>((event, emit) async {
+      final query = event.query;
+      emit(MovieSearchLoading());
+
+      final result = await _searchMovies.execute(query);
+      result.fold(
+        (failure) {
+          emit(MovieSearchError(failure.message));
+        },
+        (data) {
+          emit(MovieSearchHasData(data));
+        },
+      );
+    }, transformer: debounce(const Duration(milliseconds: 500)));
+  }
+
+  final SearchMovies _searchMovies;
+}
--- search/lib/presentation/pages/search_page.dart
+++ search/lib/presentation/pages/search_page.dart
@@ -2,13 +2,13 @@ import 'package:core/styles/text_styles.dart';
 import 'package:core/utils/state_enum.dart';
 import 'package:core/widgets/movie_card_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:search/presentation/bloc/bloc/movie_search_bloc.dart';
 
 import '../provider/movie_search_notifier.dart';
 
 class SearchPage extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +21,8 @@ class SearchPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
-              onSubmitted: (query) {
-                Provider.of<MovieSearchNotifier>(context, listen: false)
-                    .fetchMovieSearch(query);
+              onChanged: (value) {
+                context.read<MovieSearchBloc>().add(OnQueryChanged(value));
               },
               decoration: InputDecoration(
                 hintText: 'Search title',
@@ -37,19 +36,19 @@ class SearchPage extends StatelessWidget {
               'Search Result',
               style: kHeading6,
             ),
-            Consumer<MovieSearchNotifier>(
-              builder: (context, data, child) {
-                if (data.state == RequestState.Loading) {
+            BlocBuilder<MovieSearchBloc, MovieSearchState>(
+              builder: (context, state) {
+                if (state is MovieSearchLoading) {
                   return Center(
                     child: CircularProgressIndicator(),
                   );
-                } else if (data.state == RequestState.Loaded) {
-                  final result = data.searchResult;
+                } else if (state is MovieSearchHasData) {
+                  final result = state.result;
                   return Expanded(
                     child: ListView.builder(
                       padding: const EdgeInsets.all(8),
                       itemBuilder: (context, index) {
-                        final movie = data.searchResult[index];
+                        final movie = result[index];
                         return MovieCard(movie);
                       },
                       itemCount: result.length,
